3e83fe2aa4f04a5f3ace10df72e0312d259c0cb3
Kauan Propodoski <127631493+KaduTroloski@users.noreply.github.com>
2026-06-12 20:30:18 -0300

    Update server.js

--- apps/api/src/server.js
+++ apps/api/src/server.js
@@ -6,17 +6,15 @@ import { readFile } from 'fs/promises';
 import { onRequest } from 'firebase-functions/v2/https';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-if (process.env.FUNCTIONS_EMULATOR || process.env.NODE_ENV !== 'production') {
+try {
   const serviceAccount = JSON.parse(
     await readFile(new URL('./firebaseKey.json', import.meta.url))
   );
   initializeApp({ credential: cert(serviceAccount) });
-} else {
+} catch (error) {
   initializeApp();
 }
-
 app.use(express.json()); 
 
 app.use('/api', globalRoutes);
