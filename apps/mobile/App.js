9415ae466d7466e9fd68357137182ac391502bac
Emilia De Souza Colombo <emilia.48124@alunosatc.edu.br>
2026-06-15 21:24:46 -0300

    feat: integrado fluxo da tela de carregamento com confirmacao

--- apps/mobile/App.js
+++ apps/mobile/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-// Importa as suas duas telas que estão na mesma pasta
 import TelaCarregamento from './TelaCarregamento';
-import TelaConfirmacao from './TelaConfirmacao';
 
 export default function App() {
-  // Para testar a de Carregamento, troque abaixo para: <TelaCarregamento />
-  return <TelaConfirmacao />;
+  return (
+    <TelaCarregamento />
+  );
 }
